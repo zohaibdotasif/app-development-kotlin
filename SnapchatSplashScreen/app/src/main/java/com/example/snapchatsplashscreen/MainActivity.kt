d16d4dba6722ef6b9f5e6a627d53945b299c309e
Zohaib Asif <zohaibdotasif@gmail.com>
2025-02-26 12:14:44 +0500

    snapchat splash screen

---
+++ SnapchatSplashScreen/app/src/main/java/com/example/snapchatsplashscreen/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.snapchatsplashscreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
